78dc9d7f92fa1c2128819de5b854bbe1c4e09479
Keishon Smith <keishon104@gmail.com>
2017-02-25 23:55:15 -0500

    Linking functions to sms
    We have completed the functions and their compatibility with "responder"
    for buying, selling and canceling orders.

--- Robinhood_API/buy.js
+++ Robinhood_API/buy.js
@@ -1,23 +1,26 @@
 var credentials = require("./credentials");
-var Robinhood = require('robinhood')(credentials, function(){
-    var options = {
-        type: 'limit',
-        quantity: 1,
-        bid_price: 10.00,
-        instrument: {
-            url: "https:\/\/api.robinhood.com\/instruments\/1e513292-5926-4dc4-8c3d-4af6b5836704\/",
-            symbol: "SNAP"
-        }
-        // // Optional:
-        // trigger: String, // Defaults to "gfd" (Good For Day)
-        // time: String,    // Defaults to "immediate"
-        // type: String     // Defaults to "market"
-    }
-    Robinhood.place_buy_order(options, function(error, response, body){
-        if(error){
-            console.error(error);
-        }else{
-            console.log(body);
-        }
-    })
-});
+
+module.exports = () =>{
+  var Robinhood = require('robinhood')(credentials, function(){
+      var options = {
+          type: 'limit',
+          quantity: 1,
+          bid_price: 10.00,
+          instrument: {
+              url: "https:\/\/api.robinhood.com\/instruments\/1e513292-5926-4dc4-8c3d-4af6b5836704\/",
+              symbol: "SNAP"
+          }
+          // // Optional:
+          // trigger: String, // Defaults to "gfd" (Good For Day)
+          // time: String,    // Defaults to "immediate"
+          // type: String     // Defaults to "market"
+      }
+      Robinhood.place_buy_order(options, function(error, response, body){
+          if(error){
+              console.error(error);
+          }else{
+              console.log(body);
+          }
+      })
+  });
+}
--- responder.js
+++ responder.js
@@ -1,11 +1,13 @@
 var http = require('http'),
     express = require('express'),
     twilio = require('twilio'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cancel=require('./Robinhood_API/cancel');
+
+
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', function(req, res) {
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
@@ -13,6 +15,9 @@ app.get('/', function(req, res) {
         twiml.message('Hi!');
     } else if(req.query.Body == 'bye') {
         twiml.message('Goodbye');
+    } else if(req.query.Body == 'cancel order') {
+        cancel();
+
     } else {
         twiml.message('No Body param match, Twilio sends this in the request to your server.');
     }
@@ -21,16 +26,24 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
+  console.log(req.body);
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
-    if (req.body.Body == 'hello') {
-        twiml.message('Hi!');
-    } else if(req.body.Body == 'bye') {
-        twiml.message('Goodbye');
-    } else {
-        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    var body = req.body.Body;
+    if (body == 'help') {
+        twiml.message('We see you need help The commands. buy- purchase shares of stock   sell - sell your shares of stock terminate - cancel you most recent order.');
+    } else if(body == 'buy') {
+        twiml.message('You have placed a order to buy 1 shares of Snapchat,INC');
+    } else if(body == 'sell') {
+        sell();
+        twiml.message('You have placed a order to sell 1 shares of Snapchat,INC');
+    }  else if(body == 'terminate') {
+        cancel();
+        twiml.message('You have canceled our most recent order');
+    }else {
+        twiml.message('Your entry was invalid');
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 });
 
